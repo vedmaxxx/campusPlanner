27dc82abf33e9e2a0d1a3818fd04906585886e30
vedmaxxx <jidtirar@gmail.com>
2024-05-16 16:34:31 +0500

    feat: added ScheduleContext for saving state of viewMode and schedule params on forms

--- src/App.js
+++ src/App.js
@@ -1,13 +1,37 @@
 import "./styles/App.css";
 import { BrowserRouter } from "react-router-dom";
 import AppRouter from "./components/AppRouter/AppRouter";
+import NavBar from "./components/NavBar/NavBar";
+import { useEffect, useState } from "react";
+import { ScheduleContext } from "./context/ScheduleContext";
 
 function App() {
+  // состояние режима отображения редактора расписания
+  const [viewMode, setViewMode] = useState("");
+  // состояние формируемого расписания
+  const [scheduleParams, setScheduleParams] = useState("");
+  // useEffect(() => {
+  //   console.log(viewMode);
+  // }, [viewMode]);
+
   return (
     <div className="App">
-      <BrowserRouter>
-        <AppRouter />
-      </BrowserRouter>
+      {/* предоставление доступа к состояниям режима и расписания с помощью контекста */}
+      <ScheduleContext.Provider
+        value={{
+          viewMode,
+          scheduleParams,
+          setViewMode,
+          setScheduleParams,
+        }}
+      >
+        <BrowserRouter>
+          {/* ДЛЯ ОТЛАДКИ */}
+          <div className="view_mode">{viewMode} mode</div>
+          <NavBar />
+          <AppRouter />
+        </BrowserRouter>
+      </ScheduleContext.Provider>
     </div>
   );
 }
